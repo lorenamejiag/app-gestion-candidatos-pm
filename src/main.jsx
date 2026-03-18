2b5fcd19c27f802f7cbf5eb37570b523a6799f5f
lorenamejiag <loregalvan0930@gmail.com>
2026-03-17 20:32:21 -0500

    (chore): instalacion y configuracion de la dependencia react-router-dom en el archivo main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import routerApp from './routes/routerApp'
+import { createBrowserRouter, RouterProvider }  from 'react-router-dom'
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={createBrowserRouter(routerApp)} />
   </StrictMode>,
 )
